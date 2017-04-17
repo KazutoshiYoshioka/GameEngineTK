ec04ed5a2bb2a59d5b15038f55a6bf94fd0ed18a
TSITCL\s162199 <zenom1230@gmail.com>
2017-04-17 10:46:19 +0900

    DrawLineとその他色々初期化

--- GameEngineTK/Game.cpp
+++ GameEngineTK/Game.cpp
@@ -2,14 +2,27 @@
 // Game.cpp
 //
 
+//ヘッダファイルのインクルード
 #include "pch.h"
 #include "Game.h"
+
+#include <PrimitiveBatch.h>
+#include <VertexTypes.h>
+#include <Effects.h>
+#include <CommonStates.h>
+#include <SpriteBatch.h>
+#include <SimpleMath.h>
 
 extern void ExitGame();
 
 using namespace DirectX;
+using namespace SimpleMath;
 
 using Microsoft::WRL::ComPtr;
+
+std::unique_ptr<PrimitiveBatch<VertexPositionColor>> primitiveBatch;
+std::unique_ptr<BasicEffect> basicEffect;
+ComPtr<ID3D11InputLayout> inputLayout;
 
 Game::Game() :
     m_window(0),
@@ -36,6 +49,25 @@ void Game::Initialize(HWND window, int width, int height)
     m_timer.SetFixedTimeStep(true);
     m_timer.SetTargetElapsedSeconds(1.0 / 60);
     */
+
+	//初期化はここに書く
+	primitiveBatch = std::make_unique<PrimitiveBatch<VertexPositionColor>>(m_d3dContext.Get());//←コンストラクタの引数
+
+	basicEffect = std::make_unique<BasicEffect>(m_d3dDevice.Get());
+
+	basicEffect->SetProjection(XMMatrixOrthographicOffCenterRH(0,
+		m_outputWidth, m_outputHeight, 0, 0, 1));
+	basicEffect->SetVertexColorEnabled(true);
+
+	void const* shaderByteCode;
+	size_t byteCodeLength;
+
+	basicEffect->GetVertexShaderBytecode(&shaderByteCode, &byteCodeLength);
+
+	m_d3dDevice->CreateInputLayout(VertexPositionColor::InputElements,
+		VertexPositionColor::InputElementCount,
+		shaderByteCode, byteCodeLength,
+		inputLayout.GetAddressOf());
 }
 
 // Executes the basic game loop.
@@ -70,6 +102,25 @@ void Game::Render()
     Clear();
 
     // TODO: Add your rendering code here.
+	//描画はここに書く
+	CommonStates states(m_d3dDevice.Get());
+	m_d3dContext->OMSetBlendState(states.Opaque(), nullptr, 0xFFFFFFFF);
+	m_d3dContext->OMSetDepthStencilState(states.DepthNone(), 0);
+	m_d3dContext->RSSetState(states.CullNone());
+
+	basicEffect->Apply(m_d3dContext.Get());
+	m_d3dContext->IASetInputLayout(inputLayout.Get());
+
+	primitiveBatch->Begin();
+	primitiveBatch->DrawLine(VertexPositionColor(Vector3(0,0,0),Color(1.0f,1.0f,1.0f)), VertexPositionColor(Vector3(800,600,0),Color(1.0f,1.0f,1.0f)));
+	primitiveBatch->DrawQuad(
+		VertexPositionColor(Vector3(200, 100, 0), Color(1.0f, 1.0f, 1.0f)),
+		VertexPositionColor(Vector3(600, 100, 0), Color(1.0f, 1.0f, 1.0f)),
+		VertexPositionColor(Vector3(600, 500, 0), Color(1.0f, 1.0f, 1.0f)),
+		VertexPositionColor(Vector3(200, 500, 0), Color(1.0f, 1.0f, 1.0f))
+	);
+	primitiveBatch->End();
+
 
     Present();
 }
